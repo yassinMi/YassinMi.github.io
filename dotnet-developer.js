539a864037f3b7b78d62abc6b85382ae486a9cea
yassinMi <DIR16CAT17@gmail.com>
2024-08-26 12:43:45 +0100

    Fixed FOUT issue, site name, animation

--- dotnet-developer.js
+++ dotnet-developer.js
@@ -252,6 +252,7 @@ async function encryptDecrypt(elem, time) {
 async function start() {
     var dec = 120;
     var reducedMotionCodePath = window.matchMedia("(preferes-reduced-motion: reduce)").matches
+     reducedMotionCodePath = true
     var isFirstTime = true;
     while (true) {
         if (isFirstTime) {
@@ -261,7 +262,6 @@ async function start() {
             await delay(3000, 0);
         }
         currentText = { text: "Dotnet Developer", cursorPos: 16, errorDecorators: [] }
-
         isFirstTime = false;
 
         title.innerHTML = renderRichText(currentText);
@@ -270,87 +270,29 @@ async function start() {
         await backslashWithCursor(title, 6)
         await delay(120)
         await writeWithCursor(title, "Software")
-        await delay(2000);
+        await delay(1400);
         await moveCursor(title, 18, 0);
-        await delay(1000);
+        await delay(540);
         await writeWithCursor(title, ";")
-        await delay(800);
-
         await delay(100);
-
 
         currentText.errorDecorators.push({ start: 0, end: 8, type: "Error" })
         title.innerHTML = renderRichText(currentText);
-        await delay(1000);
-        if (reducedMotionCodePath) {
-            await backslashWithCursor(title, 20, 300)
-
-        }
-        if (!reducedMotionCodePath) {
-            await moveCursor(title, 8, 60);
-            await backslashWithCursor(title, 2, 300)
-            await backslashWithCursor(title, 6, 60)
-            await writeWithCursor(title, "var", 400)
-            await delay(1);
-            title.innerHTML = "<span class='keyword' >var<c>&nbsp;</c></span>Developer;"
-            await delay(3500);
-            title.innerHTML = `<span class='keyword' >var<c>&nbsp;</c></span>Developer; <span class='inline-error show'>${errors[0]}</span>`
-            await delay(300);
-
-            if (Math.random() > 0.50) {
-                title.innerHTML = `<span class='keyword' >v@<c>&nbsp;</c></span> Developer; <span class='inline-error show'>${errors[0]}</span>`
-                await delay(300);
-
-                title.innerHTML = `<span class='keyword' >ÿ<c>@</c>r</span> DþBeloperB  <span class='inline-error show'>${errors[0]}</span>`
-                await delay(300);
-
-                title.innerHTML = `<span class='keyword' ><c>ÿ</c>þB€j¤íÈ¥]</span> ${errors[0]}`
-                await delay(300);
-
-                title.innerHTML = `<span class='keyword' ><c>ÿ</c>þB€j¤íÈ¥]</span> ${errors[0]}`
-                await delay(100);
-                title.innerHTML = `<span><c>ÿ</c>þB€j¤íÈ¥]</span> ${errors[0]}`
-                await delay(100);
-                currentText = { cursorPos: 0, errorDecorators: [], text: `ÿþB€j¤íÈ¥] ${errors[0]}` };
-                title.innerHTML = renderRichText(currentText);
-                await moveCursor(title, 1, 50);
-                await moveCursorAndRandomize(title, 30, 10);
-                await moveCursorAndRandomize(title, 35, 30);
-                await moveCursorAndRandomize(title, 37, 100);
-                await delay(500, 0)
-            }
-            else {
-                currentText = { cursorPos: 0, errorDecorators: [], text: `var Developer ${errors[0]}` };
-                title.innerHTML = renderRichText(currentText);
-                await delay(1300);
-                await encryptDecrypt(title, 10);
-                await delay(800);
-
-                //await encryptDecrypt(title,10);
-                //await delay(800);
-
-            }
-
-            //await backslashWithCursor(title,500,0);
-            clearWithCursor();
-            //title.classList.remove("code")
-        }
 
         await delay(1000);
-
-        currentText = { cursorPos: 1, errorDecorators: [], text: "U" };
-        title.innerHTML = renderRichText(currentText);
-        await writeWithCursor(title, "I/UX Designer", 190);
+        await backslashWithCursor(title, 20, 50)
+        await delay(1000);
+        await writeWithCursor(title, "UI/UX Designer", 90);
 
         await delay(10000);
         await backslashWithCursor(title, 16, 80);
         await delay(500, 0)
         await writeWithCursor(title, "C# Developer")
 
-        await delay(3000, 0);
-        await backslashWithCursor(title, 16, 50);
-        await delay(500, 0)
-        await writeWithCursor(title, "WPF Developer")
+        //await delay(3000, 0);
+        //await backslashWithCursor(title, 16, 50);
+        //await delay(500, 0)
+        //await writeWithCursor(title, "WPF Developer")
 
         await delay(3000, 0);
         await backslashWithCursor(title, 16, 50);
@@ -358,11 +300,10 @@ async function start() {
         await writeWithCursor(title, "Typescript Developer")
 
         await delay(3000, 0);
-        await backslashWithCursor(title, 24, 50);
+        await backslashWithCursor(title, 22, 50);
         await delay(500, 0)
         await writeWithCursor(title, "Dotnet Developer")
         await delay(3000, 0);
-
     }
 
 }
